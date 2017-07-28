e729ce2da7b44034eb01c5ab1bfc4268f7479b7b
Jomit <jovagh@microsoft.com>
2017-07-28 14:09:25 -0700

    added testnet scripts

---
+++ testnet/startgeth.bat
@@ -0,0 +1,9 @@
+RD /S /Q %~dp0\devChain\geth\chainData
+RD /S /Q %~dp0\devChain\geth\dapp
+RD /S /Q %~dp0\devChain\geth\nodes
+del %~dp0\devchain\geth\nodekey
+
+geth.exe --datadir=devChain init genesis_dev.json
+geth.exe --rpc --networkid=191919 --cache=2048 --maxpeers=0 --datadir=devChain  --rpccorsdomain "*" --rpcapi "eth,web3,personal,net,miner,admin,debug" --verbosity 3 console
+
+REM geth.exe --mine --rpc --networkid=39319 --cache=2048 --maxpeers=0 --datadir=devChain  --rpccorsdomain "*" --rpcapi "eth,web3,personal,net,miner,admin,debug" --verbosity 3 console
